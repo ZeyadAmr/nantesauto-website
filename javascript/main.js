3a7a588eb4eb67a88f40ca8631cea45163d38ab0
Geoff <you@example.com>
2018-06-08 23:23:33 +0200

    add siema slider and add sections

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,14 @@
+// Change navbar color on scroll
 var nav = document.querySelector(".navbar");
-
 window.addEventListener("scroll", function() {
   if (window.scrollY > 10) {
     nav.classList.add("scrolled");
   } else {
     nav.classList.remove("scrolled");
   }
+});
+
+// Slider
+new Siema({
+  loop: true,
 });
