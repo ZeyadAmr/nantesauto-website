37b7757dd1ce0ffa5b1127d396d0854ff570e019
Geoff <you@example.com>
2018-06-09 20:43:21 +0200

    add images

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,12 @@
-// Change navbar color on scroll
+// Change navbar color & size on scroll
 var nav = document.querySelector(".navbar");
 window.addEventListener("scroll", function() {
-  if (window.scrollY > 10) {
+  if (window.scrollY > 20) {
     nav.classList.add("scrolled");
+    nav.classList.remove("nav-big");
   } else {
     nav.classList.remove("scrolled");
+    nav.classList.add("nav-big");
   }
 });
 
